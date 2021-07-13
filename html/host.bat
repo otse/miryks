5f5ac917519a78245243f0ec58efe96d4112b1c0
Okay <petrussijsma@gmail.com>
2021-07-13 14:50:49 +0200

    start button looks ok

--- html/host.bat
+++ html/host.bat
@@ -1,5 +1,5 @@
 REM simpler than wamp / xampp / and things like www folders
 REM -a localhost
 
-start "" http://localhost:5432/index.html
-http-server . -p 5432 -c-1
+start "" http://localhost:80/html.html
+http-server . -p 80 -c-1
