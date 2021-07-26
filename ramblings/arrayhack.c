54333c996080321cd4e02a0d4ba164f41002ef40
Okay <petrussijsma@gmail.com>
2021-07-26 14:32:19 +0200

    working on an c array hack

--- ramblings/arrayhack.c
+++ ramblings/arrayhack.c
@@ -1,4 +1,4 @@
-// Using the Developer Command Prompt, cd to Gloom/ramblings and write > cl arrayhack.c
+// usage: developer command prompt > cl arrayhack.c
 
 #include<stdio.h>
 #include<stdlib.h>
@@ -16,27 +16,83 @@ struct object
 
 struct array
 {
-	struct element ** elements;
+	void ** elements;
+	int size;
 };
 
 struct element
 {
-	int x;
+	int id;
+	const char *name;
 };
 
 typedef struct object object;
 typedef struct array array;
 typedef struct element element;
+
 typedef object * objectp;
-typedef array * arrayp;
 typedef element * elementp;
 typedef element ** elementpp;
-typedef const object * cobjectp;
+
+object * nobject();
+array * narray(int);
+
+void add(array *, element *, int);
+
+object * nobject()
+{
+	object * object = malloc(sizeof(object));
+	//object->array = narray(10);
+	return object;
+}
+
+array * narray(int size)
+{
+	array * array = malloc(sizeof(array));
+	array->size = size;
+	array->elements = calloc(size, sizeof(element *));
+	return array;
+}
+
+element * nelement()
+{
+	element * element = malloc(sizeof(element));
+	return element;
+}
+
+void add(array * array, element * element, int i)
+{
+	array->elements[i] = element;
+}
 
 #define access(p) ((elementp)(p))
 
 int main() {
-	object object;
+	object * object = nobject();
 
-	printf("one is %i", 1);
+	object->array = narray(10);
+
+	element * me = nelement();
+	me->id = 0;
+	me->name = "boo";
+
+	add(object->array, me, 0);
+
+	// cool access without the use of an obscuring macro
+	((element **)&object->array)[0]->name;
+
+	// normal access and requires a cast anyway
+	((element *)object->array->elements[0])->name;
+
+	//printf("the first element in our object's array is `%s`", ((element **)&(*object->array))[0]->name);
+
+	// here is a test with an "array of int pointers"
+
+	int **intp_array = calloc(10, sizeof(int *));
+	intp_array[2] = malloc(sizeof(int));
+	*intp_array[2] = 321;
+
+	printf("memory of intp_array        is %i\n", intp_array);
+	printf("memory of intp_array[2]     is %i\n", intp_array[2]);
+	printf("value  of intp_array[2]     is %i\n", *intp_array[2]);
 }
